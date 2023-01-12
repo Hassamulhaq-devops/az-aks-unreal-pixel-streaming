fa1c9770e42a884fc3095169972e440797363e51
Ray Kao <raykao@gmail.com>
2023-01-12 10:52:07 -0500

    update: ability to set values for nodepools

--- infrastructure/azure-cli/Nodepools.sh
+++ infrastructure/azure-cli/Nodepools.sh
@@ -1,25 +1,30 @@
+#! /bin/bash
+
+GPU_NP_SKU="Standard_NC12_Promo"
+TURN_NP_SKU="Standard_F8s_v2"
+
+# Add a GPU sku nodepool
+# Note: Taint the nodepool so that  
+
 az aks nodepool add \
-    --resource-group pixel_group \
-    --cluster-name pix \
+    --resource-group $RG_NAME \
+    --cluster-name $CLUSTER_NAME \
     --name gpunp \
     --node-count 1 \
-    --node-vm-size Standard_NC12_Promo \
-    --node-taints sku=gpu:NoSchedule \
-    --aks-custom-headers UseGPUDedicatedVHD=true \
-    --node-osdisk-size 250 \
-    --mode User 
-    
-
-az aks nodepool add \
-    --resource-group pixel_group \
-    --cluster-name pix \
-    --name turnp \
-    --node-count 1 \
-    --node-vm-size Standard_F8s_v2 \
-    --node-taints sku=turn:NoSchedule \
     --node-osdisk-size 250 \
     --mode User \
-    --enable-node-public-ip
-
-
-
+    --node-vm-size $GPU_NP_SKU \
+    --aks-custom-headers UseGPUDedicatedVHD=true \
+    --node-taints sku=gpu:NoSchedule
+    
+# Add a nodepool for TURN
+az aks nodepool add \
+    --resource-group $RG_NAME \
+    --cluster-name $CLUSTER_NAME \
+    --name turnp \
+    --node-count 1 \
+    --node-osdisk-size 250 \
+    --mode User \
+    --enable-node-public-ip \
+    --node-vm-size $TURN_NP_SKU \
+    --node-taints sku=turn:NoSchedule
