b6259cedb24f679feee4099f48733a7eb1e67068
Ray Kao <raykao@gmail.com>
2023-02-08 21:05:57 -0500

    add: create a linux dev machine to render a project as needed

--- infrastructure/azure-cli/build-jumpbox.sh
+++ infrastructure/azure-cli/build-jumpbox.sh
@@ -1,16 +1,23 @@
 #! /bin/bash
 
+# Build Jumpbox and linux dev server
+
+# XRDP
+# XFCE
+# Docker
+# https://askubuntu.com/questions/1323601/xrdp-is-quite-slow
+
 export RG_NAME="pixel_group"
 
 az vm create \
   --resource-group $RG_NAME \
   --location eastus \
-  --name gbbpixel \
+  --name gbbpixeldev \
   --image ubuntults \
   --admin-username azureuser \
   --ssh-key-value ~/.ssh/id_rsa.pub \
-  --size Standard_D16s_v3 \
+  --size Standard_NC12 \
   --os-disk-size-gb 1023 \
   --vnet-name pixel_group-vnet \
   --subnet default \
-  --public-ip-address-dns-name gbbpixel
+  --public-ip-address-dns-name gbbpixeldev
